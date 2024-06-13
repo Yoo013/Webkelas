18ba56859826c92014a95fb6d2c0ef579e7b6da2
Yoo013 <alfraka744@gmail.com>
2024-06-13 12:36:56 +0700

    homee

--- Frontend/src/components/Login/index.jsx
+++ Frontend/src/components/Login/index.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "https://webkelas-server.vercel.app/api/auth";
+      const url = "http://localhost:3000/api/auth";
       const { data: res } = await axios.post(url, data);  
 
       // console.log("Res", res);
